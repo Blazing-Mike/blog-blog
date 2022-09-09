2648e9ea7522e3400420ea9ccab77143a7db1cdc
Michael Adebambo <80793630+Blazing-Mike@users.noreply.github.com>
2022-09-09 17:56:01 +0100

    Initial commit

---
+++ src/usefetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+const[data, setData] = useState(null);
+const [isPending, setIsPending] = useState(true);
+const [error, setError] = useState(null);
+
+useEffect(() =>{
+  const abortController = new AbortController();
+    setTimeout(() =>{
+        fetch(url, {signal:abortController.signal})
+        .then(res => {
+          console.log(res);
+          if(!res.ok){
+           throw Error('could not fetch blogs');
+          }
+  
+        return res.json();
+        })
+        .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+        })
+        .catch((err) =>{
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted')
+          } else {
+            // auto catches network / connection error
+            setIsPending(false);
+            setError(err.message);
+          }
+        })
+  
+    }, 1000);
+
+    return () => abortController.abort();
+  
+  }, [url])
+
+  return {data, isPending, error};
+}
+
+export default useFetch;
